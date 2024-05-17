c10079328a3049cf7d586dd760262b7b295d6b7f
Mohammed Faisal Khan <81904023+faisalkhan4k@users.noreply.github.com>
2024-05-18 01:03:14 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,2 +1,23 @@
-a="Hello, World"
-print(a)
+import random
+
+a="ROCK PAPER SCISSOR"
+
+rock=0
+scissor=1
+paper=2
+
+choice = int(input("enter the choice 0 -> ROCK 1 -> Scissor 2-> Paper"))
+
+CPU = random.randrange(3)
+
+if(CPU == 0):
+  if(choice==0) print('draw')
+
+elif(CPU == 1):
+  if(choice==0) print('win')
+
+elif(CPU == 2):
+  if(choice==0) print('loss')
+
+else:
+  print('enter valid number')
